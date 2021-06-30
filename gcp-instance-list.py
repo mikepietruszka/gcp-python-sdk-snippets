7840e583099d0a94275cd77a4d60414899d1c287
Mike Pietruszka <mike.pietruszka@sada.com>
2021-06-30 11:38:18 -0500

    Added updates to select region instead of zone

--- gcp-instance-list.py
+++ gcp-instance-list.py
@@ -2,6 +2,9 @@
 '''
 ..moduleauthor:: Mike Pietruszka <mike@mpietruszka.com>
 ..summary:: List GCP Compute Engine instances
+..note::
+    - sys.argv[1]: GCP Project ID
+    - sys.argv[2]: Compute Engine Region
 '''
 
 import googleapiclient.discovery
@@ -9,23 +12,27 @@ import sys
 
 compute = googleapiclient.discovery.build('compute', 'v1')
 project = sys.argv[1]
-zone = sys.argv[2]
+region = sys.argv[2]
 compute_url = "https://www.googleapis.com/compute/v1/projects/"
 
 zones = compute.zones().list(project=project).execute()
-print(zones)
+our_zones = []
+for zone in zones['items']:
+    if "us-central1" in zone['region']:
+        our_zones.append(zone['name'])
 
-result = compute.instances().list(project=project, zone=zone).execute()
 print("Live instances:")
-if result.get("items"):
-    for result in result['items'] if 'items' in result else None:
-        print(result['name'],
-              result['networkInterfaces'][0]['networkIP'],
-              result['networkInterfaces'][0]['accessConfigs'][0]['natIP'],
-              result['tags']
-              )
-else:
-    print("WARN: Did not find any instances in this zone.")
+for zone in our_zones:
+    result = compute.instances().list(project=project, zone=zone).execute()
+    if result.get('items'):
+        for result in result['items'] if 'items' in result else None:
+            print(result['name'],
+                  result['networkInterfaces'][0]['networkIP'],
+                  result['networkInterfaces'][0]['accessConfigs'][0]['natIP'],
+                  result['tags']
+                  )
+    else:
+        print(f"WARN: Did not find any instances in zone: {zone}")
 
 instance_ops_request = compute.zoneOperations().list(project=project, zone=zone)
 while instance_ops_request is not None:
