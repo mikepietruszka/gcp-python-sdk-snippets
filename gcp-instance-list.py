246d58ac1719bb954cc1a2b9db0dbb475a7decee
Mike Pietruszka <mike.pietruszka@sada.com>
2021-06-30 11:31:01 -0500

    Added a script that shows you a list of instances in a given zone as well as instance operations

---
+++ gcp-instance-list.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+'''
+..moduleauthor:: Mike Pietruszka <mike@mpietruszka.com>
+..summary:: List GCP Compute Engine instances
+'''
+
+import googleapiclient.discovery
+import sys
+
+compute = googleapiclient.discovery.build('compute', 'v1')
+project = sys.argv[1]
+zone = sys.argv[2]
+compute_url = "https://www.googleapis.com/compute/v1/projects/"
+
+zones = compute.zones().list(project=project).execute()
+print(zones)
+
+result = compute.instances().list(project=project, zone=zone).execute()
+print("Live instances:")
+if result.get("items"):
+    for result in result['items'] if 'items' in result else None:
+        print(result['name'],
+              result['networkInterfaces'][0]['networkIP'],
+              result['networkInterfaces'][0]['accessConfigs'][0]['natIP'],
+              result['tags']
+              )
+else:
+    print("WARN: Did not find any instances in this zone.")
+
+instance_ops_request = compute.zoneOperations().list(project=project, zone=zone)
+while instance_ops_request is not None:
+    print("\nInstance operations:")
+    instance_ops_result = instance_ops_request.execute()
+
+    for result in instance_ops_result['items'] \
+            if 'items' in instance_ops_result else None:
+        print(result['name'],
+              result['operationType'],
+              result['targetLink'].replace(
+                  f"{compute_url}{project}/", ''),
+              result['status'],
+              result['progress']
+              )
+    instance_ops_request = compute.zoneOperations().list_next(
+        previous_request=instance_ops_request,
+        previous_response=instance_ops_result
+    )
