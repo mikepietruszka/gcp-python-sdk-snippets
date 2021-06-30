d4191d15d25c07cdc62209149b5a6039eeabf995
Mike Pietruszka <mike.pietruszka@sada.com>
2021-06-30 11:25:13 -0500

    Added a script to pull Service Account information from IAM

---
+++ gcp-iam-sa-list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+'''
+..moduleauthor:: Mike Pietruszka <mike@mpietruszka.com>
+..summary:: Set project name as your sys.argv[1] CLI argument
+'''
+
+import googleapiclient.discovery
+import sys
+
+iam = googleapiclient.discovery.build('iam', 'v1')
+project = sys.argv[1]
+compute_url = "https://www.googleapis.com/compute/v1/projects/"
+
+iam_roles = iam.projects().roles().list(parent=f"projects/{project}").execute()
+print("IAM Roles:")
+for role in iam_roles['roles']:
+    print(role)
+
+print("\nIAM Service Accounts:")
+service_accounts = iam.projects().serviceAccounts().list(name=f"projects/{project}").execute()
+for sa in service_accounts['accounts']:
+    print(sa)
+    key = iam.projects().serviceAccounts().keys().list(name=sa['name']).execute()
+    print(key)
+
